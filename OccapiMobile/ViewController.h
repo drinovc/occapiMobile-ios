a0129a1b43bad0bf8dbdac8acff4707a40776aca
Rok Drinovec <drinovc@gmail.com>
2014-01-05 15:43:05 -0800

    Initial Commit

---
+++ OccapiMobile/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OccapiMobile
+//
+//  Created by Rok Drinovec on 05/01/14.
+//  Copyright (c) 2014 Rok Drinovec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
