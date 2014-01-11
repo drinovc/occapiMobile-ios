836d262be03326f24d3275af65ac71df1e3aaa67
Rok Drinovec <drinovc@gmail.com>
2014-01-11 22:47:01 +0100

    global vars login an json parsing

---
+++ OccapiMobile/DataClass.h
@@ -0,0 +1,13 @@
+//
+//  DataClass.h
+//  OccapiMobile
+//
+//  Created by Rok Drinovec on 11/01/14.
+//  Copyright (c) 2014 Rok Drinovec. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataClass : NSObject
+
+@end
---
+++ OccapiMobile/DataClass.m
@@ -0,0 +1,13 @@
+//
+//  DataClass.m
+//  OccapiMobile
+//
+//  Created by Rok Drinovec on 11/01/14.
+//  Copyright (c) 2014 Rok Drinovec. All rights reserved.
+//
+
+#import "DataClass.h"
+
+@implementation DataClass
+
+@end
